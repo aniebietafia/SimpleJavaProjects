dd01926ed0e4d522e1ecc436f0fe6ab44007d8b9
aniebietafia <aniebietafia87@gmail.com>
2024-07-29 10:44:20 +0100

    quota calculator

--- SimpleTasks/src/main/java/quota_calculator/QuotaCalculator.java
+++ SimpleTasks/src/main/java/quota_calculator/QuotaCalculator.java
@@ -13,10 +13,17 @@ public class QuotaCalculator {
         int no_of_sales = scanner.nextInt();
         scanner.close();
 
-        if (no_of_sales < avg_sales) {
+        if (no_of_sales < 0) {
+            System.out.println("Sales cannot be less than 0");
+            return;
+        }
+
+        if (no_of_sales == 0) {
+            message = "You didn't make any sales this week.";
+        } else if (no_of_sales < avg_sales) {
             int sales_short = avg_sales - no_of_sales;
             message = sales_short == 1 ? "You are " + sales_short + " sale short" : "You are " + sales_short + " sales short";
-        } else {
+        }  else {
             message = "Congratulations! You met your target for the week.";
         }
 
