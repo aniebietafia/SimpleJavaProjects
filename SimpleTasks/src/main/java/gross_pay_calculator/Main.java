9b34ad2abbc34bbfca854ff2387453d9435cdad4
aniebietafia <aniebietafia87@gmail.com>
2024-07-29 08:36:51 +0100

    gross pay calculator

---
+++ SimpleTasks/src/main/java/gross_pay_calculator/Main.java
@@ -0,0 +1,8 @@
+package gross_pay_calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        GrossPayCalculator gross_pay = new GrossPayCalculator();
+        gross_pay.execute();
+    }
+}
