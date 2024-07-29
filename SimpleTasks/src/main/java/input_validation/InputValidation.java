05e996c2f9e59b73d7f21a414246f1538175e1f5
aniebietafia <aniebietafia87@gmail.com>
2024-07-29 15:57:22 +0100

    gross pay calculator

---
+++ SimpleTasks/src/main/java/input_validation/InputValidation.java
@@ -0,0 +1,27 @@
+package input_validation;
+
+import java.util.Scanner;
+
+public class InputValidation {
+    public String input_validation() {
+        int hourlyPay = 15;
+        int maxHours = 40;
+        int grossPay = 0;
+
+        System.out.println("How many hours have you worked this week?");
+        Scanner scanner = new Scanner(System.in);
+        int hoursWorked = scanner.nextInt();
+
+        while (hoursWorked > maxHours) {
+            System.out.println("Invalid entry.");
+            System.out.println("How many hours have you worked this week?");
+            hoursWorked = scanner.nextInt();
+
+        }
+
+        scanner.close();
+
+        grossPay = hourlyPay * hoursWorked;
+        return "Your gross pay for the week is " + grossPay;
+    }
+}
---
+++ SimpleTasks/src/main/java/input_validation/Main.java
@@ -0,0 +1,8 @@
+package input_validation;
+
+public class Main {
+    public static void main(String[] args) {
+        InputValidation inputValidation = new InputValidation();
+        System.out.println(inputValidation.input_validation());
+    }
+}
