1e6fd53c63ccde837e4a8603cf8a4e7665220439
aniebietafia <aniebietafia87@gmail.com>
2024-08-04 20:23:01 +0100

    implementing inheritance

---
+++ SimpleTasks/src/main/java/inhritance/Employee.java
@@ -0,0 +1,22 @@
+package inhritance;
+
+public class Employee extends Person{
+    private String employeeId;
+    private String jobTitle;
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+}
