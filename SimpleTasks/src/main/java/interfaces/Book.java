4612e2e80587eb7e4c8c7aa0b3523999a7361f0a
aniebietafia <aniebietafia87@gmail.com>
2024-08-12 16:59:19 +0100

    implementing interfaces

---
+++ SimpleTasks/src/main/java/interfaces/Book.java
@@ -0,0 +1,43 @@
+package interfaces;
+
+public class Book implements Product{
+
+    private String name;
+    private String author;
+    private int price;
+    private String isbn;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ SimpleTasks/src/main/java/interfaces/Main.java
@@ -0,0 +1,8 @@
+package interfaces;
+
+public class Main {
+    public static void main(String[] args) {
+        Product book = new Book();
+        book.setName("The Salvation that Jesus gives.");
+    }
+}
