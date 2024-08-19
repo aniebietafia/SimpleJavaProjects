cdadc51650f747e7a5ac4f625fefb01b888d6601
aniebietafia <aniebietafia87@gmail.com>
2024-08-19 13:35:00 +0100

    file handling and try with resources

--- SimpleTasks/src/main/java/exceptions/MultipleExceptionHandling.java
+++ SimpleTasks/src/main/java/exceptions/MultipleExceptionHandling.java
@@ -2,17 +2,23 @@ package exceptions;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MultipleExceptionHandling {
     public void readFile() {
         File file = new File("files/numbers.txt");
+        File outputFile = new File("files/output.txt");
         //Scanner fileReader = null;
 
-        try(Scanner fileReader = new Scanner(file);) {
+        try(
+                Scanner fileReader = new Scanner(file);
+                PrintWriter fileWriter = new PrintWriter(outputFile)
+        ) {
             while (fileReader.hasNext()) {
-                System.out.println(fileReader.nextDouble());
+                //System.out.println(fileReader.nextDouble());
+                fileWriter.println(fileReader.nextDouble());
             }
         } catch (FileNotFoundException | InputMismatchException error) {
             System.out.println(error.getMessage());
